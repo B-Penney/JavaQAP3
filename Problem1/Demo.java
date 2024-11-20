9748fc1d17a9f0ca80057d508c7013d4d841fd78
B-Penney <162143485+B-Penney@users.noreply.github.com>
2024-11-20 17:56:58 -0330

    Added Files to Problem1. Added some code to demo file

---
+++ Problem1/Demo.java
@@ -0,0 +1,9 @@
+package Problem1;
+
+public class Demo {
+    public static void main(String[] args) {
+        Student student = new Student();
+        Teacher teacher = new Teacher();
+        CollegeStudent collegeStudent = new CollegeStudent();
+    }
+}
